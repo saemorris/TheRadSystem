0067b2fc6652b910b0bc68079f307250acafda95
ItsTristan <tmeleshk@ualberta.ca>
2015-03-10 11:10:22 -0600

    Added index page

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php require('session.php'); ?>
+
+<html>
+    <body>
+        <h1>
+            Success!
+        </h1>
+        <p>
+            If you can read this after signing in, logging in works.
+        </p>
+        <p><a href="logout.php">Log out</a></p>
+    </body>
+</html>
