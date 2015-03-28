0ac53a5a863da19a79bea42bb9698d5a58564eb4
saemorris <semorris@ualberta.ca>
2015-03-28 11:19:24 -0600

    Date range search functional

--- _search.php
+++ _search.php
@@ -13,35 +13,44 @@ if (isset($_POST['search'])) {
 		
 		//check if date query
 		if (empty($_POST['keyWordQuery'])) {
-				
-			$fromDate = $_POST['dateQueryFrom'];
-			$toDate = $_POST['dateQueryTo'];
 			
-			$query="SELECT * FROM radiology_record";
+			// query the database for records in the specified date range	
+			$startDate = $_POST['dateQueryFrom'];
+			$endDate = $_POST['dateQueryTo'];
+			$query="SELECT * FROM radiology_record WHERE prescribing_date BETWEEN to_date('$startDate', 'YYYY-MM-DD') and to_date('$endDate', 'YYYY-MM-DD')";
 			
 			$statement = oci_parse($connection, $query);
 
 			$results = oci_execute($statement);
-	
+			
 			// Did we get a valid result?
 			if ($results) {
 				// display the results
 				echo "<table border='1' cellspacing=0>";
 				
+				// get the number of fields in the table
+				$numCols = oci_num_fields($statement);
+				
+				// display column headers
+				echo "<tr>";
+				for ($i = 1; $i <= $numCols; $i++) {
+					$column_name = oci_field_name($statement, $i);
+					
+					echo"<th>$column_name</th>";
+				}
+				echo "</tr>";
+				
+				// display the rows found for the specified date range
 				while ($row = oci_fetch_array($statement, OCI_ASSOC+OCI_RETURNS_NULLS)) {
 				
 					echo "<tr>";
-					
 					foreach($row as $item) {
-						//echo "<td>" . ($item !== null ? htmlentities($item, ENT_QUOTES) : "&nbsp;") . "</td>";
-						echo "<td>" . $item . "</td>";
+						echo "<td>" . ($item !== null ? htmlentities($item, ENT_QUOTES) : "&nbsp;") . "</td>";
 					}
-
 					echo "</tr>";
 				}
 				echo "</table>";
 				
-
 			} else {
 				// No.
 				// Inform the user
