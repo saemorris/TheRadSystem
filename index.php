529ad291f4e81652a36ae81cbda0ad7493d7c8c8
Tristan Meleshko <tmeleshk@ualberta.ca>
2015-03-30 14:04:48 -0600

    Fixed user data not being available

--- index.php
+++ index.php
@@ -8,6 +8,11 @@
         <p>
             If you can read this after signing in, logging in works.
         </p>
+        <p>Your username is "<?php echo getUserName() ?>"</p>
+        <p>Your account type is "<?php echo getUserClass() ?>"</p>
+        <p>Your id is "<?php echo getUserPersonID() ?>"</p>
+        <p>Your session was created at <?php echo date("M d, Y g:i:s a", $_SESSION['us_created_time']) ?></p>
+        <p>Your session was last active at <?php echo date("M d, Y g:i:s a", $_SESSION['us_last_activity']) ?></p>
         <p><a href="logout.php">Log out</a></p>
     </body>
 </html>
