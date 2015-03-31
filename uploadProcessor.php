99482109a8750aebd8d4a7736535f848d6134bb2
saemorris <semorris@ualberta.ca>
2015-03-31 12:11:04 -0600

    can now view pictures

--- uploadProcessor.php
+++ uploadProcessor.php
@@ -5,22 +5,15 @@
 // make a note of the current working directory, relative to root. 
 $directory_self = str_replace(basename($_SERVER['PHP_SELF']), '', $_SERVER['PHP_SELF']); 
 
-echo $directory_self . "<p>";
-
 // make a note of the directory that will recieve the uploaded file 
 $uploadsDirectory = $_SERVER['DOCUMENT_ROOT'] . $directory_self . 'uploaded_files/'; 
-
-echo $uploadsDirectory . "<p>";
 
 // make a note of the location of the upload form in case we need it 
 $uploadForm = 'http://' . $_SERVER['HTTP_HOST'] . $directory_self . 'uploadImage.php'; 
 
-echo $uploadForm . "<p>";
 
 // make a note of the location of the success page 
 $uploadSuccess = 'http://' . $_SERVER['HTTP_HOST'] . $directory_self . 'uploadSuccess.php'; 
-
-echo $uploadSuccess . "<p>";
 
 // fieldname used within the file <input> of the HTML form 
 $filename = 'image_file'; 
@@ -63,23 +56,23 @@ echo "uploadFileName: " . $uploadFilename . "<p>";
 //require a connection to the database
 require ('_database.php');
 
-//$image = addslashes(file_get_contents("starfish.jpeg"));
-
 // Creates an "empty" OCI-Lob object to bind to the locator
 $image_lob = oci_new_descriptor($connection, OCI_D_LOB);
 
-$query="INSERT INTO pacs_images (record_id, image_id, regular_size) VALUES (1, image_id_seq.nextval, EMPTY_BLOB()) 
-	RETURNING regular_size INTO :image";
+$query="INSERT INTO pacs_images (record_id, image_id, full_size) VALUES (1, image_id_seq.nextval, EMPTY_BLOB()) 
+	RETURNING full_size, image_id INTO :image, :id";
 
 $statement = oci_parse($connection, $query);
 
 // Bind the returned Oracle LOB locator to the PHP LOB object
 oci_bind_by_name($statement, ":image", $image_lob, -1, OCI_B_BLOB);
 
+oci_bind_by_name($statement, ":id", $id);
+
 // Execute the statement using , OCI_DEFAULT - as a transaction
 oci_execute($statement, OCI_DEFAULT) or die ("Unable to execute query\n");
 
-echo "filename: " . $_FILES[$filename]['name'] . "<p>";
+//echo "filename: " . $_FILES[$filename]['name'] . "<p>";
 
 if($image_lob->savefile($_FILES[$filename]['name'])) {
 	oci_commit($connection);
@@ -88,50 +81,11 @@ if($image_lob->savefile($_FILES[$filename]['name'])) {
 	echo "Couldn't upload image";
 }
 
-echo "<img src='displayImage.php?id=61' />";
-
-/*
-// display the picture that was uploaded
-$query = "SELECT regular_size from pacs_images WHERE image_id = 9";
-$statement = oci_parse($connection, $query);
-$results = oci_execute($statement);
-$row_data = oci_fetch_row($statement);
-
-if(!$row_data[0]) {
-	echo "No Image!";
-} else {
-	$image = $row_data[0]->load();
-}
-*/
-/*
-$temp = tmpfile();
-$meta = stream_get_meta_data($temp);
-$tmpName = $meta['uri'];
-fwrite($temp, $image);
-fclose($temp);
-
-echo "<img src=" . $tmpName . "/>"; */
-/*
-$img = imagecreate(100,100); //Create an image 100px x 100px
-imagecolorallocate($img, 255,0,0); //Fill the image red
-header('Content-type: image/jpeg'); //Set the content type to image/jpg
-imagejpeg($img); //Output the iamge
-imagedestroy($img); //Destroy the image
-
-echo "<img src='image.php' />";
-*/
+echo "<img src='displayImage.php?id=$id' />";
 
 // Clean up database objects
 oci_free_statement($statement);
 oci_close($connection);
-
-// now let's move the file to its final location and allocate the new filename to it 
-//@move_uploaded_file($_FILES[$fieldname]['tmp_name'], $uploadFilename) 
-//   or error('receiving directory insuffiecient permission', $uploadForm); 
-     
-// If you got this far, everything has worked and the file has been successfully saved. 
-// We are now going to redirect the client to a success page. 
-//header('Location: ' . $uploadSuccess); 
 
 // The following function is an error handler which is used 
 // to output an HTML error page if the file upload fails 
