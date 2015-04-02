0b4067de0530e9d360a77b0d95f2be718678023f
Tristan Meleshko <tmeleshk@ualberta.ca>
2015-04-02 13:03:02 -0600

    Expanded IDs into details

--- record.php
+++ record.php
@@ -24,31 +24,55 @@ $default = "displayImage.php?id=$id&size=";
 		<?php
 		require("_database.php");
 		
-		$query = "SELECT record_id, patient_id, doctor_id, radiologist_id, ". 
+		$query = "SELECT record_id, ". 
+			"p.first_name p_first_name, p.last_name p_last_name, p.address p_address, p.email p_email, p.phone p_phone, ".
+			"d.first_name d_first_name, d.last_name d_last_name, d.address d_address, d.email d_email, d.phone d_phone, ".
+			"r.first_name r_first_name, r.last_name r_last_name, r.address r_address, r.email r_email, r.phone r_phone, ". 
 			"test_type, prescribing_date, test_date, diagnosis, description ".
-			"FROM radiology_record WHERE record_id = :id";
+			"FROM radiology_record rec, persons p, persons d, persons r ".
+			"WHERE	".
+			"		record_id = :id AND ".
+			"		rec.patient_id = p.person_id AND ".
+			"		rec.doctor_id = d.person_id AND ".
+			"		rec.radiologist_id = r.person_id";
 		
 		$statement = oci_parse($connection, $query);
 		
 		oci_bind_by_name($statement, ":id", $id);
 		
-		echo $query;
-		
 		if (oci_execute($statement)) {
 			if (oci_fetch($statement)) { ?>
-				<h3><?php echo oci_result($statement, 'RECORD_ID'); ?></h3>
-				<table>
+				<h3>Record <?php echo oci_result($statement, 'RECORD_ID'); ?></h3>
+				<table border="2">
 					<tr>
-						<td>Patient ID</td>
-						<td><?php echo oci_result($statement, 'PATIENT_ID'); ?></td>
+						<td>Patient</td>
+						<td><table border="1">
+							<tr><td>First Name</td>	<td><?php echo oci_result($statement, 'P_FIRST_NAME'); ?></td></tr>
+							<tr><td>Last Name</td>	<td><?php echo oci_result($statement, 'P_LAST_NAME'); ?></td></tr>
+							<tr><td>Address</td>	<td><?php echo oci_result($statement, 'P_ADDRESS'); ?></td></tr>
+							<tr><td>Email</td> 		<td><?php echo oci_result($statement, 'P_EMAIL'); ?></td></tr>
+							<tr><td>Phone</td>  	<td><?php echo oci_result($statement, 'P_PHONE'); ?></td></tr>
+						</table></td>
 					</tr>
 					<tr>
-						<td>Doctor ID</td>
-						<td><?php echo oci_result($statement, 'DOCTOR_ID'); ?></td>
+						<td>Doctor</td>
+						<td><table border="1">
+							<tr><td>First Name</td>	<td><?php echo oci_result($statement, 'D_FIRST_NAME'); ?></td></tr>
+							<tr><td>Last Name</td>	<td><?php echo oci_result($statement, 'D_LAST_NAME'); ?></td></tr>
+							<tr><td>Address</td>	<td><?php echo oci_result($statement, 'D_ADDRESS'); ?></td></tr>
+							<tr><td>Email</td> 		<td><?php echo oci_result($statement, 'D_EMAIL'); ?></td></tr>
+							<tr><td>Phone</td>  	<td><?php echo oci_result($statement, 'D_PHONE'); ?></td></tr>
+						</table></td>
 					</tr>
 					<tr>
-						<td>Radiologist ID</td>
-						<td><?php echo oci_result($statement, 'RADIOLOGIST_ID'); ?></td>
+						<td>Radiologist</td>
+						<td><table border="1">
+							<tr><td>First Name</td>	<td><?php echo oci_result($statement, 'R_FIRST_NAME'); ?></td></tr>
+							<tr><td>Last Name</td>	<td><?php echo oci_result($statement, 'R_LAST_NAME'); ?></td></tr>
+							<tr><td>Address</td>	<td><?php echo oci_result($statement, 'R_ADDRESS'); ?></td></tr>
+							<tr><td>Email</td> 		<td><?php echo oci_result($statement, 'R_EMAIL'); ?></td></tr>
+							<tr><td>Phone</td>  	<td><?php echo oci_result($statement, 'R_PHONE'); ?></td></tr>
+						</table></td>
 					</tr>
 					<tr>
 						<td>Test Type</td>
@@ -91,7 +115,7 @@ $default = "displayImage.php?id=$id&size=";
 		while (oci_fetch($statement)) { 
 			$index = oci_result($statement, 'IMAGE_ID');
 			echo "<p><img id='pacimg$index' imgsize='1' base='displayImage.php?id=$index' ".
-					"src='displayImage.php?id=$index' onclick=\"zoompac('pacimg$index')\" /></p>";
+					"src='displayImage.php?id=$index' onclick=\"zoompac('pacimg$index')\" /></p>\n";
 		}
 		
 		oci_free_statement($statement); 
